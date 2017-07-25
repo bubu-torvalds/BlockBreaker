cc99f70249a933138c18aaf50fd506401e6e3f83
Maxime Buttard <maxime.buttard@gmail.com>
2017-07-25 10:49:55 +0200

    add error log

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -56,6 +56,8 @@ public class Brick : MonoBehaviour {
 		int spriteIndex = timesHit - 1;
 		if (hitSprites[spriteIndex]) {
 			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		} else {
+			Debug.LogError("No Sprite to render. Brick sprite missing.");
 		}
 	}
 	
